ddfb89a218fdfc4247a0977d6388b21f9e5e261d
Matias Marzullo <matias.marzu@gmail.com>
2017-04-06 09:46:26 -0300

    prueba json

---
+++ js/auto.js
@@ -0,0 +1,27 @@
+var jsonTiposChasis = [
+        { 
+          "tipo": "Corsa",
+          "img": "url_img_corsa"
+          "id": "1" 
+        },{
+         "tipo": "KA",
+          "img": "url_img_ka" 
+          "id": "2"
+        },{ 
+          "tipo": "Uno",
+          "img": "url_img_uno"
+          "id": "3" 
+        }
+        ];
+
+tipos_tazas = [
+        { "tipo": "Nombre taza 1",
+          "img": "url_img" 
+        },{ 
+          "tipo": "Nombre taza 2",
+          "img": "url_img" 
+        },{ 
+          "tipo": "Nombre taza 3",
+          "img": "url_img" 
+        }
+        ];
