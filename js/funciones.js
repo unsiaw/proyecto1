2b9d8e4755d853f831bfeeab23d7956466e435cd
Ricardo <rferromoreno@gmail.com>
2017-04-09 12:56:37 -0300

    Agregué función para cargar imagen random

--- js/funciones.js
+++ js/funciones.js
@@ -17,3 +17,23 @@ function cargarImagen() {
     console.log("Estoy cargando la imagen");
 }
 
+
+function cargarRandom() {
+    var canvas = document.getElementById('imagenAuto');
+    var context = canvas.getContext('2d');
+
+    var imagenes = [ "auto1.png", "corsa.jpg", "ka.jpg",  "uno.jpg"];
+
+    var randomNumber = Math.floor((Math.random() * imagenes.length));
+
+    img = new Image();
+    img.src = 'img/'+ imagenes[randomNumber];
+    img.onload = function () {
+        canvas.width = img.width;
+        canvas.height = img.height;
+        context.drawImage(img, 0, 0, img.width, img.height);
+    }
+    console.log("Estoy cargando la imagen random");
+    console.log(img.src);
+}
+
