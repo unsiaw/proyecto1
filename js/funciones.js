f248a08ac2fdf82f96dc49b171ea9c846c63fcef
matiaschap <matias.marzu@gmail.com>
2017-04-10 16:10:23 -0300

    next and prev chasis

--- js/funciones.js
+++ js/funciones.js
@@ -4,6 +4,48 @@
 const path_chasis = "img/chasis/";
 const path_tazas = "img/tazas/";
 
+   var imagenes = [ "auto1.png", "car08.png", "car09.png",  "car010.png", "car011.png",  "car012.png", "car013.png",  "car014.png"];
+   var num = 0;
+
+function nextChasis(){
+
+    var canvas = document.getElementById('imagenAuto');
+    var context = canvas.getContext('2d');
+    img = new Image();
+    num++;
+        if (num >= imagenes.lenght){
+            num = 0;
+        }
+    img.src = path_chasis + imagenes[num];
+    img.onload = function () {
+        canvas.width = img.width;
+        canvas.height = img.height;
+        context.drawImage(img, 0, 0, img.width, img.height);
+    }
+    console.log("Estoy cargando la imagen siguiente");
+    console.log(img.src);
+}
+
+
+function prevChasis(){
+
+    var canvas = document.getElementById('imagenAuto');
+    var context = canvas.getContext('2d');
+    img = new Image();
+    num--;
+        if (num < imagenes.lenght){
+            num = imagenes.lenght - 1;
+        }
+    img.src = path_chasis + imagenes[num];
+    img.onload = function () {
+        canvas.width = img.width;
+        canvas.height = img.height;
+        context.drawImage(img, 0, 0, img.width, img.height);
+    }
+    console.log("Estoy cargando la imagen anterior");
+    console.log(img.src);
+}
+
 function cargarImagen() {
     var canvas = document.getElementById('imagenAuto');
     var context = canvas.getContext('2d');
@@ -61,47 +103,3 @@ function numeroRandom(limite) {
 $().ready(function() {
     cargarRandom();
 });
-
-
-/*   var imagenes = [ "auto1.png", "car08.png", "car09.png",  "car010.png", "car011.png",  "car012.png", "car013.png",  "car014.png"];
-    var num = 0;
-
-function nextChasis(){
-
-    var canvas = document.getElementById('imagenAuto');
-    var context = canvas.getContext('2d');
-    img = new Image();
-    num++;
-        if (num >= imagenes.lenght){
-            num = 0;
-        }
-    img.src = path_chasis + imagenes[num];
-    img.onload = function () {
-        canvas.width = img.width;
-        canvas.height = img.height;
-        context.drawImage(img, 0, 0, img.width, img.height);
-    }
-    console.log("Estoy cargando la imagen siguiente");
-    console.log(img.src);
-}
-
-
-function prevChasis(){
-
-    var canvas = document.getElementById('imagenAuto');
-    var context = canvas.getContext('2d');
-    img = new Image();
-    num-;
-        if (num < imagenes.lenght){
-            num = imagenes.lenght - 1;
-        }
-    img.src = path_chasis + imagenes[num];
-    img.onload = function () {
-        canvas.width = img.width;
-        canvas.height = img.height;
-        context.drawImage(img, 0, 0, img.width, img.height);
-    }
-    console.log("Estoy cargando la imagen anterior");
-    console.log(img.src);
-}
-*/
