fdafddb7217c29dab3ab0233f9cdf5e741a1fb24
Ricardo <rferromoreno@gmail.com>
2017-04-16 00:41:04 -0300

    Se puede colorear auto. Hay que arreglarlo en Chrome

--- js/funciones.js
+++ js/funciones.js
@@ -7,6 +7,15 @@ const path_tazas = "img/tazas/";
 var imagenes = [ "auto1.png", "car08.png", "car09.png",  "car010.png", "car011.png",  "car012.png", "car013.png",  "car014.png"];
 var num = 0;
 
+var autito = {};
+
+var canvas = new fabric.Canvas('imagenAuto');
+
+canvas.setWidth(miContenedor.offsetWidth);
+canvas.setHeight(miContenedor.offsetHeight * 2);
+canvas.renderAll();
+
+
 function nextChasis(){
 
     var canvas = document.getElementById('imagenAuto');
@@ -65,15 +74,15 @@ function cargarRandom() {
         canvas.width = img.width;
         canvas.height = img.height;
         context.drawImage(img, 0, 0, img.width, img.height);
-    }
+    };
     console.log("Estoy cargando la imagen random");
     console.log(img.src);
 }
 
 function cambiarColor() {
 
-    var canvas = new fabric.Canvas('imagenAuto');
-
+    //var canvas = new fabric.Canvas('imagenAuto');
+/*
     fabric.Image.fromURL(path_chasis + imagenes[num], function(img) {
         img.filters.push(new fabric.Image.filters.Blend({
             color: document.getElementById('blend-color').value,
@@ -85,6 +94,7 @@ function cambiarColor() {
 
         console.log("Estoy cambiando el color");
     });
+    */
 }
 
 /* Crea un numero random entre 0 y un limite dado */
@@ -94,7 +104,7 @@ function numeroRandom(limite) {
 
 $().ready(function() {
     cargarTheme();
-    cargarRandom();
+    cargarAuto();
 });
 
 /* Themes propios y guardando el theme elegido */
@@ -110,4 +120,53 @@ function cargarTheme() {
 function cambiarTheme(theme) {
     document.getElementById('theme').href = theme;
     localStorage.setItem("theme",theme);
+}
+
+
+/* Pruebas nuevas sobre carga de un auto, ruedas, chasis, etc */
+function cargarAuto() {
+
+    var miContenedor = document.getElementById("miContenedor");
+
+    fabric.Image.fromURL("img/chasis/car01.png", function(img) {
+        img.selectable = false;
+        img.crossOrigin = 'anonymous';
+        canvas.add(img);
+    }, {crossOrigin: ''});
+    fabric.Image.fromURL("img/chasis/car01tra.png", function(img) {
+        img.selectable = false;
+        img.crossOrigin = 'anonymous';
+        canvas.add(img);
+        autito["chasis"] = img;
+    }, {crossOrigin: ''});
+
+    canvas.renderAll();
+}
+
+
+function cambiarColor(elemento){
+    var obj = autito["chasis"];
+
+    console.log(obj);
+
+    obj.filters = []; // reseteo los filtros (para no sobreescribir uno arriba del otro)
+    obj.applyFilters();
+
+    obj.filters.push(new fabric.Image.filters.Tint({
+        color: document.getElementById('blend-color').value,
+        opacity: 0.6
+    }));
+
+
+    /*
+     obj.filters.push(new fabric.Image.filters.Blend({
+     color: document.getElementById('blend-color').value,
+     mode: 'add',
+     alpha: 1
+     }));
+     */
+    obj.applyFilters(canvas.renderAll.bind(canvas));
+
+    canvas.renderAll();
+    //applyFilterValue(16, 'color', this.value);
 }
