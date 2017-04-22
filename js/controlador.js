172ac253f1506b98aa8cc44518a358bbbf9f18df
Ricardo <rferromoreno@gmail.com>
2017-04-22 15:17:21 -0300

    El auto se guarda en localstorage

--- js/controlador.js
+++ js/controlador.js
@@ -7,30 +7,50 @@ var jsonChasis;
 var jsonTazas;
 
 function iniciarAuto() {
-    num_chasis = 0;
-    num_taza = 0;
+    num_chasis = localStorage.getItem("num_chasis");
+    if ((num_chasis === undefined) || (num_chasis === null)) {
+        num_chasis = 0;
+    }
+    num_taza = localStorage.getItem("num_taza");
+    if ((num_taza === undefined) || (num_taza === null)) {
+        num_taza = 0;
+    }
+    var color = localStorage.getItem("color");
+    if ((color === undefined) || (color === null)) {
+        auto.color = obtenerColorVista();
+    } else {
+        auto.color = color;
+        cambiarColorVista(color);
+    }
     auto.chasis = jsonChasis[num_chasis];
     auto.tazas = jsonTazas[num_taza];
-    auto.color = obtenerColorVista();
     pintarAuto(auto);
+    // Guardo datos en el localStorage
+    localStorage.setItem("color",auto.color);
+    localStorage.setItem("num_chasis",num_chasis);
+    localStorage.setItem("num_taza",num_taza);
 }
 
 function cambiarColor(color) {
     auto.color = color;
     cambiarColorVista(color);
     pintarAuto(auto);
+    localStorage.setItem("color",color);
 }
 
 function nextChasis() {
     num_chasis = mod((num_chasis+1),jsonChasis.length);
     auto.chasis = jsonChasis[num_chasis];
     pintarAuto(auto);
+    localStorage.setItem("num_chasis",num_chasis);
+
 }
 
 function prevChasis() {
     num_chasis = mod((num_chasis-1),jsonChasis.length);
     auto.chasis = jsonChasis[num_chasis];
     pintarAuto(auto);
+    localStorage.setItem("num_chasis",num_chasis);
 }
 
 
@@ -38,12 +58,14 @@ function nextTaza() {
     num_taza = mod((num_taza+1),jsonTazas.length);
     auto.tazas = jsonTazas[num_taza];
     pintarAuto(auto);
+    localStorage.setItem("num_taza",num_taza);
 }
 
 function prevTaza() {
     num_taza = mod((num_taza-1),jsonTazas.length);
     auto.tazas = jsonTazas[num_taza];
     pintarAuto(auto);
+    localStorage.setItem("num_taza",num_taza);
 }
 
 function cargarRandom() {
@@ -54,6 +76,10 @@ function cargarRandom() {
     auto.chasis = jsonChasis[num_chasis];
     auto.tazas = jsonTazas[num_taza];
     pintarAuto(auto);
+    // Guardo datos en el localStorage
+    localStorage.setItem("color",color);
+    localStorage.setItem("num_chasis",num_chasis);
+    localStorage.setItem("num_taza",num_taza);
 }
 
 /* Functionalidad para el botón de Descargar Imagen */
